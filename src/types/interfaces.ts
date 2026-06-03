324a152d4c1c810739101ffe7503613555b4e11e
Leandro de Oliveira Santos <shyllenno@gmail.com>
2026-06-03 19:49:29 +0100

    Movie Details page components.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -21,3 +21,9 @@ export interface BaseMovieListProps {
     movies: BaseMovieProps[];
 }
 
+export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+        id: number;
+        name: string;
+    }[];
+}
