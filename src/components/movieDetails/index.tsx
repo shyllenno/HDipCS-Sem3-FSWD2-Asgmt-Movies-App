dc01a650cfd23a8ed7fd19238c1b0ea3499f46b4
Leandro de Oliveira Santos <shyllenno@gmail.com>
2026-06-15 22:13:19 +0100

    Show movie review excerpts; Refactor Datails page.

--- src/components/movieDetails/index.tsx
+++ src/components/movieDetails/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -6,67 +6,80 @@ import MonetizationIcon from "@mui/icons-material/MonetizationOn";
 import StarRate from "@mui/icons-material/StarRate";
 import Typography from "@mui/material/Typography";
 import { MovieDetailsProps } from "../../types/interfaces";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from '../movieReviews'
 
 const styles = {
-    chipSet: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-        listStyle: "none",
-        padding: 1.5,
-        margin: 0,
-    },
-    chipLabel: {
-        margin: 0.5,
-    },
+  chipSet: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: 1.5,
+    margin: 0,
+  },
+  chipLabel: {
+    margin: 0.5,
+  },
+  fab: {
+    position: "fixed",
+    top: 50,
+    right: 2,
+  },
 };
 
 const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
 
-    return (
-        <>
-            <Typography variant="h5" component="h3">
-                Overview
-            </Typography>
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
-            <Typography variant="h6" component="p">
-                {movie.overview}
-            </Typography>
+  return (
+    <>
+      <Typography variant="h5" component="h3">
+        Overview
+      </Typography>
 
-            <Paper component="ul" sx={styles.chipSet}>
-                <li>
-                    <Chip label="Genres" sx={styles.chipLabel} color="primary" />
-                </li>
-                {movie.genres.map((g) => (
-                    <li key={g.name}>
-                        <Chip label={g.name} />
-                    </li>
-                ))}
-            </Paper>
-            <Paper component="ul" sx={styles.chipSet}>
-                <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-                <Chip
-                    icon={<MonetizationIcon />}
-                    label={`${movie.revenue.toLocaleString()}`}
-                />
-                <Chip
-                    icon={<StarRate />}
-                    label={`${movie.vote_average} (${movie.vote_count}`}
-                />
-                <Chip label={`Released: ${movie.release_date}`} />
-            </Paper>
-            <Paper component="ul" sx={styles.chipSet}>
-                <li>
-                    <Chip label="Production Countries" sx={styles.chipLabel} color="primary" />
-                </li>
-                {movie.production_countries.map((c) => (
-                    <li key={c.name}>
-                        <Chip label={c.name} />
-                    </li>
-                ))}
-            </Paper>
-        </>
-    );
+      <Typography variant="h6" component="p">
+        {movie.overview}
+      </Typography>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip label="Genres" sx={styles.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" sx={styles.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count}`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        sx={styles.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews {...movie} />
+      </Drawer>
+    </>
+  );
 };
 export default MovieDetails;
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -50,3 +50,9 @@ export type FilterOption = "title" | "genre";
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
     title: string;
 }
+
+export interface Review {
+    id: string;
+    content: string;
+    author: string;
+}
